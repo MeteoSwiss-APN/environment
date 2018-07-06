c61e3c8c0015f19b2f0292dc292337127720aef5
Felix Thaler <thaler@cscs.ch>
2018-07-06 09:55:22 +0200

    Fix Tave GNU environment for MPI

--- env.tave.gnu.sh
+++ env.tave.gnu.sh
@@ -6,13 +6,11 @@ else
     module swap ${prgenv} PrgEnv-gnu
 fi
 
+module load cray-mpich
 module load CMake
 
 # Boost
 export BOOST_ROOT=/scratch/snx2000/jenkins/install/boost/boost_1_64_0
 export LD_LIBRARY_PATH=${BOOST_ROOT}/lib:${LD_LIBRARY_PATH}
 
-# Override C++ and C compiler
-export CXX=`which g++`
-export CC=`which gcc`
 export FC=ftn
