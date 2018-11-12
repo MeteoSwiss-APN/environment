d1b4d5c3e965d635fdb6c0eb5bc29f4dc21cbd21
Hannes Vogt <hannes@havogt.de>
2018-11-12 09:17:03 +0100

    Update env.daint.pgi.sh

--- env.daint.pgi.sh
+++ env.daint.pgi.sh
@@ -9,7 +9,7 @@ else
     module swap ${prgenv} PrgEnv-pgi
 fi
 
-module load /users/jenkins/easybuild/daint/haswell/modules/all/CMake/3.12.0
+module load /users/mlukas/easybuild/daint/haswell/modules/all/CMake/3.12.4
 module load craype-accel-nvidia60
 
 module unload pgi
